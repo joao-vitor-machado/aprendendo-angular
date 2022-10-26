773a2bcbf65de554ec44fa4795dd1ab86afa8027
João Vitor Leite Machado <joao.machado@ekinops.com>
2022-10-26 10:42:06 -0300

    Integrando uma API mockada para realizar o get de transferências

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NovaTransferenciaComponent } from './nova_transferencia/nova_transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
@@ -21,7 +22,8 @@ import localePt from '@angular/common/locales/pt';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt'},
--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,4 +1,7 @@
+import { Transferencia } from './../models/transferencia-model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,13 +9,18 @@ import { Injectable } from '@angular/core';
 export class TransferenciaService {
 
 private listaTransferencias: any[];
+private url = "http://localhost:3000/transferencias";
 
-constructor() {
+constructor(private httpClient : HttpClient) {
   this.listaTransferencias = [];
  }
 
  get transferencias() {
   return this.listaTransferencias;
+ }
+
+ todas() : Observable<Array<Transferencia>>{
+  return this.httpClient.get<Array<Transferencia>>(this.url);
  }
 
  adicionar(transferencia: any) {
